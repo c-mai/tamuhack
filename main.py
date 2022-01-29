2c6ff661284b6bda01340e1b7010b715dbc31f75
c-mai <51352051+c-mai@users.noreply.github.com>
2022-01-29 16:32:19 -0600

    basic app

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
+
+@app.route('/')
+def main():
+   return render_template('index.html')
   
 @app.route('/success/<name>')
 def success(name):
